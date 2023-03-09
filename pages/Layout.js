fcbf6f36e618fec87dd2d64ff47eeee9bade831f
mamun07 <mamunben7@gmail.com>
2023-03-10 02:22:59 +0600

    sdf

---
+++ pages/Layout.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      Header
+      {children}
+      Footer
+    </>
+  );
+};
+
+export default Layout;
---
+++ pages/contactUs/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ContactUs = () => {
+  return (
+    <>
+      <h2>ContactUs</h2>
+    </>
+  );
+};
+
+export default ContactUs;
