4411affecefad6ce30a1c4ae3bfe6e3bb003e9a6
mamun07 <mamunben7@gmail.com>
2023-03-10 02:36:46 +0600

    talwin use

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,4 @@
 import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
 
 export default function Home() {
   return (
@@ -12,7 +9,11 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>ok</main>
+      <main>
+        <h1 className="text-3xl font-bold underline">
+          Welcome to Just Natural
+        </h1>
+      </main>
     </>
   );
 }
