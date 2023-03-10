c727928f13ec65c0a61740a6793622f7878ad322
mamun07 <mamunben7@gmail.com>
2023-03-10 11:35:58 +0600

    sfs

---
+++ sections/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <div>Footer Top</div>
+      <div>Footer Bottom</div>
+    </>
+  );
+};
+
+export default Footer;
---
+++ sections/Header/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="text">
+      <h2>Header Nav bar</h2>
+    </div>
+  );
+};
+
+export default Header;
