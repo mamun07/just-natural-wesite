986774fa496dd0a62a07e7d35ee30e833c288e07
mamun07 <mamunben7@gmail.com>
2023-03-10 22:13:19 +0600

    footer done

--- sections/Footer/index.js
+++ sections/Footer/index.js
@@ -1,22 +1,198 @@
 import React from "react";
-import { Col, Container, Row } from "react-bootstrap";
-import { FooterMain } from "./style";
+import Link from "next/link";
+import { Container, Col, Row } from "react-bootstrap";
+import {
+  FooterBottom,
+  FooterTop,
+  FooterMid,
+  MiddleMenu,
+  MiddleSocial,
+  WidgetBottomMenu,
+} from "./style";
+
+import { IoLogoFacebook, IoLogoTwitter, IoLogoInstagram } from "react-icons/io";
 
 const Footer = () => {
   return (
-    <FooterMain>
-      <Container>
-        <Row>
-          <Col xs={12}>
-            <div>Footer Top</div>
-          </Col>
-          <Col xs={12}>
-            <div>Footer Bottom</div>
-          </Col>
-        </Row>
-      </Container>
-    </FooterMain>
+    <>
+      <FooterTop>
+        <Container>
+          <Row>
+            {widgetData?.map((widget, i) => {
+              return (
+                <Col key={i} xs={12} md={3}>
+                  <h4>{widget.title}</h4>
+                  <ul>
+                    {widget?.menu.map((menu, i) => {
+                      return (
+                        <li key={i}>
+                          <Link href={menu.link}>{menu.label}</Link>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </Col>
+              );
+            })}
+
+            {/* Widget Bottom Menu */}
+            <Col xs={12}>
+              <WidgetBottomMenu>
+                <ul>
+                  {widgetBottomMenu?.map((list, i) => {
+                    return (
+                      <li key={i}>
+                        <Link href={list.link}>{list.title}</Link>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </WidgetBottomMenu>
+            </Col>
+          </Row>
+        </Container>
+      </FooterTop>
+      <FooterMid>
+        <Container fluid>
+          <Row justify-content-center>
+            <Col xs={12} md={5}>
+              <MiddleMenu>
+                <ul>
+                  <li>
+                    <Link href="/">Pitch deck</Link>
+                  </li>
+                  <li>
+                    <Link href="/">privacy policy</Link>
+                  </li>
+                  <li>
+                    <Link href="/">terms of use</Link>
+                  </li>
+                  <li>
+                    <Link href="/">cookie policy</Link>
+                  </li>
+                </ul>
+              </MiddleMenu>
+            </Col>
+            <Col xs={12} md={3}>
+              <MiddleSocial>
+                <ul>
+                  <li>
+                    <Link href="/">
+                      <IoLogoFacebook />
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/">
+                      <IoLogoInstagram />
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/">
+                      <IoLogoTwitter />
+                    </Link>
+                  </li>
+                </ul>
+              </MiddleSocial>
+            </Col>
+            <Col xs={12} md={4}>
+              <p>© 2023 Just Natural Corporation. All right reserved.</p>
+            </Col>
+          </Row>
+        </Container>
+      </FooterMid>
+      <FooterBottom>
+        <Container fluid>
+          <Row>
+            <Col xs={12}>
+              <p>Nothing more to look for</p>
+            </Col>
+          </Row>
+        </Container>
+      </FooterBottom>
+    </>
   );
 };
 
 export default Footer;
+
+const widgetData = [
+  {
+    title: "A glimpse on us",
+    menu: [
+      {
+        label: "Our Story",
+        link: "/",
+      },
+      {
+        label: "Our Journey",
+        link: "/",
+      },
+      {
+        label: "Our Team",
+        link: "/",
+      },
+      {
+        label: "Our Certification",
+        link: "/",
+      },
+      {
+        label: "Our Packaging",
+        link: "/",
+      },
+      {
+        label: "Our Factory",
+        link: "/",
+      },
+    ],
+  },
+  {
+    title: "Things we do",
+    menu: [
+      {
+        label: "Snacks",
+        link: "/",
+      },
+      {
+        label: "Honey",
+        link: "/",
+      },
+      {
+        label: "Instant Drinks",
+        link: "/",
+      },
+    ],
+  },
+  {
+    title: "Things we do for the society",
+    menu: [
+      {
+        label: "Our Story",
+        link: "/",
+      },
+    ],
+  },
+  {
+    title: "What people think of us",
+    menu: [
+      {
+        label: "Our Story",
+        link: "/",
+      },
+    ],
+  },
+];
+
+const widgetBottomMenu = [
+  {
+    title: "Wanna work with us?",
+    link: "/",
+  },
+  {
+    title: "Get in Touch",
+    link: "/",
+  },
+  {
+    title: "Blog",
+    link: "/",
+  },
+];
--- sections/Footer/style.js
+++ sections/Footer/style.js
@@ -1,7 +1,119 @@
 import styled from "styled-components";
 
-export const FooterMain = styled.div`
+export const FooterTop = styled.div`
   background: var(--brand-color);
   color: var(--secondary-color);
+  padding: var(--p100) 0;
+  h4 {
+    font-size: 18px;
+    line-height: 24px;
+    font-weight: 700;
+    margin-bottom: 15px;
+  }
+  ul {
+    margin: 0;
+    padding: 0;
+    li {
+      list-style: none;
+      margin-right: 30px;
+      :last-child {
+        margin-right: 0;
+      }
+      a {
+        color: var(--secondary-color);
+        text-decoration: none;
+        display: block;
+        :hover {
+          text-decoration: underline;
+        }
+      }
+    }
+  }
+`;
+
+export const FooterMid = styled.div`
+  background: var(--brand1-color);
+  text-align: center;
+  color: var(--secondary-color);
   padding: var(--py15);
+  p {
+    margin: 0;
+  }
+`;
+export const FooterBottom = styled.div`
+  text-align: center;
+  padding: var(--py15);
+  p {
+    margin: 0;
+  }
+`;
+export const MiddleMenu = styled.div`
+  ul {
+    margin: 0;
+    padding: 0;
+    li {
+      list-style: none;
+      float: left;
+      margin-right: 30px;
+      :last-child {
+        margin-right: 0;
+      }
+      a {
+        color: var(--secondary-color);
+        text-decoration: none;
+        display: block;
+        :hover {
+          text-decoration: underline;
+        }
+      }
+    }
+  }
+`;
+export const MiddleSocial = styled.div`
+  ul {
+    margin: 0;
+    padding: 0;
+    li {
+      list-style: none;
+      float: left;
+      margin-right: 30px;
+      :last-child {
+        margin-right: 0;
+      }
+      a {
+        color: var(--secondary-color);
+        text-decoration: none;
+        display: block;
+        svg {
+          font-size: 30px;
+        }
+      }
+    }
+  }
+`;
+export const WidgetBottomMenu = styled.div`
+  padding-top: var(--p40);
+  ul {
+    margin: 0;
+    padding: 0;
+    li {
+      list-style: none;
+      float: left;
+      margin-right: 30px;
+      :last-child {
+        margin-right: 0;
+      }
+      a {
+        color: var(--secondary-color);
+        text-decoration: none;
+        display: block;
+        font-size: 18px;
+        line-height: 24px;
+        font-weight: 700;
+        :hover {
+          text-decoration: underline;
+        }
+      }
+    }
+  }
 `;
